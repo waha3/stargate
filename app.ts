eb52c73c95e50e62e6b2a2aa66b6e11671a0b4b4
han <895240740@qq.com>
2023-05-23 19:37:34 +0800

    add

--- app.ts
+++ app.ts
@@ -1,22 +1,20 @@
-import { Builder, By, until } from "selenium-webdriver";
-import { ServiceBuilder } from "selenium-webdriver/chrome.js";
-
-const service = new ServiceBuilder("/Users/hanxin/chromedriver");
-const driver = new Builder().forBrowser("chrome").setChromeService(service).build();
+import { Builder, By, WebDriver } from "selenium-webdriver";
+import robot from "robotjs";
 
 // 模拟人类的行为 区间在 页面加载好的 [5 - 15]s内
 function getRandomInt(min: number = 5, max: number = 15): number {
   min = Math.ceil(min);
   max = Math.floor(max);
-  // return (Math.floor(Math.random() * (max - min + 1)) + min) * 1000;
-  return 1000;
+  return (Math.floor(Math.random() * (max - min + 1)) + min) * 1000;
 }
 
-async function inital() {
+async function inital(): Promise<WebDriver> {
+  const driver = new Builder().forBrowser("firefox").build();
   await driver.manage().setTimeouts({ implicit: 10000 });
-  // await driver.manage().window().setSize(1000, 800);
+  return driver;
 }
-async function installMetaMask() {
+
+async function installMetaMask(driver: WebDriver) {
   await driver.get("https://metamask.io/");
 
   let meta_mask_download_btn = await driver.findElement(
@@ -25,15 +23,27 @@ async function installMetaMask() {
   await driver.sleep(getRandomInt());
   await meta_mask_download_btn.click();
 
-  let add_to_chrome_btn = await driver.wait(until.elementLocated(By.css(".PNF6le")));
+  // 等待
+  await driver.sleep(15000);
 
-  // let add_to_chrome_btn = await driver.findElement(By.css("PNF6le"));
-  // await driver.sleep(getRandomInt());
-  // await add_to_chrome_btn.click();
-  console.log("add to chrome", add_to_chrome_btn);
+  // 
+  robot.moveMouseSmooth(1120, 220);
+  robot.mouseClick();
+
+  await driver.sleep(500);
+  robot.moveMouseSmooth(1200, 240);
+  robot.mouseClick();
+
+  await driver.sleep(500);
+  robot.moveMouseSmooth(896, 197);
+  robot.mouseClick();
+
+  await driver.sleep(500);
+  robot.moveMouseSmooth(1228, 242);
+  robot.mouseClick();
 }
 
-async function stargate() {
+async function stargate(driver: WebDriver) {
   await driver.get("https://stargate.finance/transfer");
   let connectWalletButton = await driver.findElement(
     By.xpath('//*[contains(@class, "MuiButton-label") and contains(text(), "Connect Wallet")]')
@@ -49,8 +59,9 @@ async function stargate() {
 }
 
 async function run() {
-  await inital();
-  await installMetaMask();
+  let driver = await inital();
+  await installMetaMask(driver);
+  await stargate(driver);
 }
 
 run();

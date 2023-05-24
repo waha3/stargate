cc65640fbed6428ffa4115e96376dfe958fc68c4
han <895240740@qq.com>
2023-05-24 16:07:25 +0800

    add

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 import { Builder, By, WebDriver } from "selenium-webdriver";
 import robot from "robotjs";
+import { keys, password } from "./privatekeys/mnemonic";
 
-// 模拟人类的行为 区间在 页面加载好的 [5 - 15]s内
-function getRandomInt(min: number = 5, max: number = 15): number {
+// 模拟人类的行为 区间在 页面加载好的 [5 - 10]s内
+function getRandomInt(min: number = 2, max: number = 2): number {
   min = Math.ceil(min);
   max = Math.floor(max);
   return (Math.floor(Math.random() * (max - min + 1)) + min) * 1000;
@@ -23,10 +24,10 @@ async function installMetaMask(driver: WebDriver) {
   await driver.sleep(getRandomInt());
   await meta_mask_download_btn.click();
 
-  // 等待
-  await driver.sleep(15000);
+  // 等待15s 让metamask插件下载完成 （可能会超过这个时间根据自己网络手动调节）
+  await driver.sleep(20000);
 
-  // 
+  // 确认安装 metamask （浏览器原生的元素）
   robot.moveMouseSmooth(1120, 220);
   robot.mouseClick();
 
@@ -41,6 +42,58 @@ async function installMetaMask(driver: WebDriver) {
   await driver.sleep(500);
   robot.moveMouseSmooth(1228, 242);
   robot.mouseClick();
+}
+async function importWallet(driver: WebDriver) {
+  // 点击浏览器应用商店的入口
+  await driver.sleep(500);
+  robot.moveMouseSmooth(1225, 90);
+  robot.mouseClick();
+
+  // 选择metamask
+  await driver.sleep(500);
+  robot.moveMouseSmooth(1030, 180);
+  robot.mouseClick();
+  await driver.sleep(getRandomInt());
+
+  // 切换tab
+  let windows = await driver.getAllWindowHandles();
+  await driver.switchTo().window(windows[1]);
+
+  let importWalletBtn = await driver.findElement(
+    By.xpath('//button[contains(text(), "Import an existing wallet")]')
+  );
+  await driver.sleep(getRandomInt());
+  await importWalletBtn.click();
+
+  let agreeBtn = await driver.findElement(By.xpath('//button[contains(text(), "I agree")]'));
+  await driver.sleep(getRandomInt());
+  await agreeBtn.click();
+
+  // 通过input的id来定位对应的助记词位置（可能网页后面id名字会变）
+  for (let [index, key] of keys.entries()) {
+    await driver.findElement(By.id(`import-srp__srp-word-${index}`)).sendKeys(key);
+  }
+
+  // submit
+  let confirmImportWalletBtn = await driver.findElement(
+    By.xpath('//button[contains(text(), "Confirm Secret Recovery Phrase")]')
+  );
+  await driver.sleep(getRandomInt());
+  await confirmImportWalletBtn.click();
+
+  // 创建密码
+  await driver.findElement(By.css('input[data-testid="create-password-new"]')).sendKeys(password);
+  await driver
+    .findElement(By.css('input[data-testid="create-password-confirm"]'))
+    .sendKeys(password);
+  let confirmSetPasswordCheckbox = await driver.findElement(
+    By.css('input[data-testid="create-password-terms"]')
+  );
+  await confirmSetPasswordCheckbox.click();
+  let confirmSetPasswordBtn = await driver.findElement(
+    By.css('input[data-testid="create-password-import"]')
+  );
+  await confirmSetPasswordBtn.click();
 }
 
 async function stargate(driver: WebDriver) {
@@ -61,7 +114,8 @@ async function stargate(driver: WebDriver) {
 async function run() {
   let driver = await inital();
   await installMetaMask(driver);
-  await stargate(driver);
+  await importWallet(driver);
+  // await stargate(driver);
 }
 
 run();
